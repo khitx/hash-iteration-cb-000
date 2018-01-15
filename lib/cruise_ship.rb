d0467d04b35246460f256a9f34546c5082c47293
Kathy Hudson <khitx@icloud.com>
2018-01-15 18:25:55 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,3 +15,13 @@ def select_winner(passengers)
   end
   winner
 end
+
+birthday_kids = {
+  'Timmy' => 9,
+  'Sarah' => 6,
+  'Amanda' => 27
+}
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each { |name, age| puts "#{name}! You are now #{age} years old!" }
+end
